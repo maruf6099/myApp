5aed7df03f925508e59351efb0dc353cc1d842ab
maruf <marufbdn609@gmail.com>
2025-03-14 19:44:44 +0600

    showIndex_page

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//resource Route
+Route::resource('blog', BlogController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
